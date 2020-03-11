05acf46aa80148ab026351c21b58afcea82c6e3c
GyaneshMahto <61919561+Gyanesh-Mahto@users.noreply.github.com>
2020-03-11 23:14:28 +0530

    Add files via upload

---
+++ 02_Foundation/05_Sending_output_to_console.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  // TODO: Add your code here:
+  cout<<"Hello"<<endl;
+}
+
+/*
+O/P:
+Hello
+*/
---
+++ 02_Foundation/08_primitive_variable_type.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=5;
+    float f=9.5f;
+    double d=9.67;
+    string s="Hello";
+
+    cout<<"Int a: "<<a<<endl;
+    cout<<"Float f: "<<f<<endl;
+    cout<<"Double d: "<<d<<endl;
+    cout<<"String s: "<<s<<endl;
+}
