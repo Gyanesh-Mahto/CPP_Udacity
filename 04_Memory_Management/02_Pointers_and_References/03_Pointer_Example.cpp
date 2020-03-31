fb218db782f01dcd398906439d517e4014199c8b
Gyanesh Mahto <61919561+Gyanesh-Mahto@users.noreply.github.com>
2020-03-31 23:32:11 +0530

    Update 03_Pointer_Example.cpp

--- 04_Memory_Management/02_Pointers_and_References/03_Pointer_Example.cpp
+++ 04_Memory_Management/02_Pointers_and_References/03_Pointer_Example.cpp
@@ -9,11 +9,13 @@ int main()
 
     cout<<"Value of ptr(Address): "<<ptr<<endl;
     cout<<"Value of val: "<<val<<endl;
+    cout<<"Address of val: "<<&val<<endl;
     cout<<"Value of *ptr(Value): "<<*ptr<<endl;
 }
 /*
 O/P:
 Value of ptr(Address): 0x7ffd4aed289c
 Value of val: 5
+Address of val: 0x7ffd4aed289c
 Value of *ptr(Value): 5
 */
