1de3e3c8dc22e953d954c268029d360351796cbf
Gyanesh Mahto <61919561+Gyanesh-Mahto@users.noreply.github.com>
2020-04-07 23:40:48 +0530

    Add files via upload

---
+++ 03_Object_Oriented_Programming/03_Advanced_OOP/03_Inheritance/main.cpp
@@ -0,0 +1,8 @@
+/*
+Inheritence
+In our everyday life, we tend to divide things into groups, based on their shared characteristics. Here are some groups that you have probably used yourself: electronics, tools, vehicles, or plants.
+
+Sometimes these groups have hierarchies. For example, computers and smartphones are both types of electronics, but computers and smartphones are also groups in and of themselves. You can imagine a tree with "electronics" at the top, and "computers" and "smartphones" each as children of the "electronics" node.
+
+
+*/
