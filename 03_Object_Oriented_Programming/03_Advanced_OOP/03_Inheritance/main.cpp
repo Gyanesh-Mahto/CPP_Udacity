1b69cf6edd311b70e5f2b344f7174f81fce2a2a8
Gyanesh Mahto <61919561+Gyanesh-Mahto@users.noreply.github.com>
2020-04-08 22:00:16 +0530

    Update main.cpp

--- 03_Object_Oriented_Programming/03_Advanced_OOP/03_Inheritance/main.cpp
+++ 03_Object_Oriented_Programming/03_Advanced_OOP/03_Inheritance/main.cpp
@@ -1,8 +1,69 @@
 /*
-Inheritence
-In our everyday life, we tend to divide things into groups, based on their shared characteristics. Here are some groups that you have probably used yourself: electronics, tools, vehicles, or plants.
+Instructions
+Add a new member variable to class Vehicle.
+Output that new member in main().
+Derive a new class from Vehicle, alongside Car and Bicycle.
+Instantiate an object of that new class.
+Print the object.
+*/
+#include<iostream>
+using namespace std;
 
-Sometimes these groups have hierarchies. For example, computers and smartphones are both types of electronics, but computers and smartphones are also groups in and of themselves. You can imagine a tree with "electronics" at the top, and "computers" and "smartphones" each as children of the "electronics" node.
+class Vehicle
+{
+    private:
+    string model_name;
 
+    public:
+    int wheels = 0;
+    int weight=0;
+    string color = "blue";
+    void SetName(string n)
+    {
+        model_name=n;
+    }
+    void Print()
+    {
+        cout<<"This "<<model_name<<" vehicle "<<" of "<<color <<" color "<<" has "<<wheels<<" wheels and total weight is "<<weight<<" Kgs"<<endl;
+    }
+};
 
+class Car: public Vehicle
+{
+    public:
+    Car()
+    {}
+
+};
+
+class Bicycle : public Vehicle
+{
+    public:
+    Bicycle()
+    {}
+};
+
+int main()
+{
+    Vehicle v1;
+    v1.Print();
+    Car c1;
+    c1.SetName("Maruti");
+    c1.color="Red";
+    c1.weight=4000;
+    c1.wheels=4;
+    c1.Print();
+    Bicycle b1;
+    b1.SetName("Ranger");
+    b1.color="Black";
+    b1.weight=50;
+    b1.wheels=2;
+    b1.Print();
+}
+
+/*
+O/P:
+This  vehicle  of blue color  has 0 wheels and total weight is 0 Kgs
+This Maruti vehicle  of Red color  has 4 wheels and total weight is 4000 Kgs
+This Ranger vehicle  of Black color  has 2 wheels and total weight is 50 Kgs
 */
