f3819e85584bad0c3eda880720264efbfd9e48c3
Gyanesh Mahto <61919561+Gyanesh-Mahto@users.noreply.github.com>
2020-04-18 23:25:28 +0530

    Update ProcessParser.h

--- 03_Object_Oriented_Programming/04_Project_System_Monitor/ProcessParser.h
+++ 03_Object_Oriented_Programming/04_Project_System_Monitor/ProcessParser.h
@@ -16,7 +16,8 @@
 #include<dirent.h>
 #include<time.h>
 #include<unistd.h>
-
+#include "Util.h"
+#include "constants.h"
 using namespace std;
 
 class ProcessParser
@@ -38,3 +39,33 @@ class ProcessParser
     static string getOsName();
     static string printCpuStats(vector<string> values1, vector<string> values2);
 };
+
+//Reading /proc/[PID]/status for memory status of specific process
+string ProcessParser::getVmSize(string pid)
+{
+    string line;
+    //Declaring search attribute for file
+    string name="VmData";
+    string value;
+    float result;
+
+    //opening stream for specific file
+    ifstream stream=Util::getStream((Path::basePath()+ pid + Path::statusPath()));  
+
+    while(getline(stream, line))
+    {
+        //searching line by line
+        if(line.compare(0, name.size(), name)==0)
+        {
+            //slicing string line on ws for values using sstream
+            istringstream buf(line);
+            istream_iterator<string> beg(buf), end;
+            vector<string> values(beg, end);
+
+            //conversion KB->GB
+            result=(stof(values[1])/float(1024));
+            break;
+        }
+    };
+    return to_string(result);
+}
