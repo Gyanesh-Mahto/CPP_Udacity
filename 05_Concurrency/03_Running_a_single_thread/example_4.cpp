7d8a867bbddcb93443448582de034d35b1a955d0
Gyanesh Mahto <61919561+Gyanesh-Mahto@users.noreply.github.com>
2020-05-18 23:57:54 +0530

    Add files via upload

---
+++ 05_Concurrency/03_Running_a_single_thread/example_4.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<thread>
+using namespace std;
+
+void thread_func()
+{
+    this_thread::sleep_for(chrono::milliseconds(50));  //Simulate work
+    cout<<"Finished work 1 in thread"<<endl;
+    this_thread::sleep_for(chrono::milliseconds(50));  //Simulate work
+    cout<<"Finished work 2 in thread"<<endl;
+}
+
+int main()
+{
+    thread t(thread_func);
+    this_thread::sleep_for(chrono::milliseconds(50));  //Simulate work
+    cout<<"Finished work 1 in main"<<endl;
+    this_thread::sleep_for(chrono::milliseconds(50));  //Simulate work
+    cout<<"Finished work 2 in main"<<endl;
+    t.join();
+}
+
+/*
+-->Compile with command: g++ -std=c++11 -pthread ./example_3.cpp
+-->./a.out
+Output is not fixed.
+O/P:
+Finished work 1 in main
+Finished work 1 in thread
+Finished work 2 in thread
+Finished work 2 in main
+*/
