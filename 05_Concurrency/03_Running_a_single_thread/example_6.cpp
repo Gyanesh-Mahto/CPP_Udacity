3f1eea27cd31500f3cbd7456483701e8c267a5b6
Gyanesh Mahto <61919561+Gyanesh-Mahto@users.noreply.github.com>
2020-05-19 10:59:14 +0530

    Add files via upload

---
+++ 05_Concurrency/03_Running_a_single_thread/example_6.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<thread>
+using namespace std;
+
+void thread_func()
+{
+    //Do some task in thread
+    this_thread::sleep_for(chrono::milliseconds(50));   //Simulate work
+    cout<<"Finished task in thread"<<endl;
+}
+
+int main()
+{
+    //Creating thread
+    thread t(thread_func);
+
+    //Using detach: Thread will run separately from main thread
+    t.detach();
+
+    //Do some task in main
+    this_thread::sleep_for(chrono::milliseconds(50));   //Simulate work
+    cout<<"Finished task in main"<<endl;
+}
+/*
+For Linux Environment:
+-->Compile with command: g++ -std=c++11 -pthread ./example_6.cpp
+-->./a.out
+
+For Windows Environment:
+-->Compile with command: g++ -std=c++11 ./example_6.cpp
+-->./a.out
+
+O/P is not fixed
+O/P:
+Finished task in thread
+Finished task in main
+*/
